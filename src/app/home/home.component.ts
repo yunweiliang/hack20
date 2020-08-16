356e1433a31090cef32b65ed93b0f6485f978528
Jaimie Jin <jinjaimie12@gmail.com>
2020-08-16 10:44:49 -0700

    shortening home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  cardList = [
+    {
+      routerLink: "['/memories']",
+      header: "Memorial",
+      content1: "Visit John Smith’s memorial page",
+      content2: "Share photos & videos",
+    },
+    {
+      routerLink: "['/fundraise']",
+      header: "Fundraiser",
+      content1: "Share fundraiser to social media",
+      content2: " Donate to support the family",
+    },
+    {
+      routerLink: "['/zoom']",
+      header: "Zoom",
+      content1: "Schedule Counseling Sessions",
+      content2: "Attend Funeral Service",
+    },
+    {
+      routerLink: "['/resources']",
+      header: "Mental Health",
+      content1: "Learn more about grief and acceptance",
+      content2: "Connect to counselors",
+    }
+  ];
+  name = "Jane"
   constructor() { }
 
   ngOnInit(): void {
