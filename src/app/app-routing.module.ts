688bd9c820e0b24018220ba598f68d210918bfc7
Jaimie Jin <jinjaimie12@gmail.com>
2020-08-15 17:10:54 -0700

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FundraiserComponent } from './fundraiser/fundraiser.component';
 import { LoginComponent } from './login/login.component';
+import { ResourcesComponent } from './resources/resources.component';
+import { MemoryShareComponent } from './memory-share/memory-share.component';
 
 const routes: Routes = [
   { path: 'fundraise', component: FundraiserComponent},
-  { path: 'login', component: LoginComponent}
+  { path: 'login', component: LoginComponent},
+  { path: 'resources', component: ResourcesComponent},
+  { path: 'memories', component: MemoryShareComponent}
 ];
 
 @NgModule({
